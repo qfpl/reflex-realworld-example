ff249a6c91e7ee87b0b80299a2c96dbc50b9fc35
Ben Kolera <ben.kolera@gmail.com>
2019-03-01 16:24:52 +1000

    Settings form.

--- frontend/src/Frontend/Settings.hs
+++ frontend/src/Frontend/Settings.hs
@@ -7,9 +7,54 @@ module Frontend.Settings where
 
 import           Reflex.Dom.Core
 
+import qualified Data.Map        as Map
+
 settings
   :: ( DomBuilder t m
+     , PostBuild t m
+     , Prerender js m
      )
   => m ()
 settings = elClass "div" "settings-page" $ do
-  blank
+  elClass "div" "container page" $
+    elClass "div" "row" $
+      elClass "div" "col-md-6 offset-md-3 col-xs-12" $ do
+        elClass "h1" "text-xs-center" $ text "Your Settings"
+        prerender blank $ el "form" $
+          el "fieldset" $ do
+            _ <- elClass "fieldset" "form-group" $
+              textInput $ def
+                & textInputConfig_attributes .~ (constDyn (Map.fromList
+                  [("class","form-control")
+                  ,("placeholder","URL of profile picture")
+                  ]))
+            _ <- elClass "fieldset" "form-group" $
+              textInput $ def
+                & textInputConfig_attributes .~ (constDyn (Map.fromList
+                  [("class","form-control")
+                  ,("placeholder","Your name")
+                  ]))
+            _ <- elClass "fieldset" "form-group" $
+              textArea $ def
+                & textAreaConfig_attributes .~ (constDyn (Map.fromList
+                  [("class","form-control")
+                  ,("placeholder","Short bio about you")
+                  ,("rows","8")
+                  ]))
+
+            _ <- elClass "fieldset" "form-group" $
+              textInput $ def
+                & textInputConfig_inputType .~ "email"
+                & textInputConfig_attributes .~ (constDyn (Map.fromList
+                  [("class","form-control")
+                  ,("placeholder","Email")
+                  ]))
+            _ <- elClass "fieldset" "form-group" $
+              textInput $ def
+                & textInputConfig_inputType .~ "password"
+                & textInputConfig_attributes .~ (constDyn (Map.fromList
+                  [("class","form-control")
+                  ,("placeholder","Password")
+                  ]))
+            (_,_) <- elClass' "button" "btn btn-lg btn-primary pull-xs-right" $ text "Update Settings"
+            pure ()
