3176bd23d43fcefffe374fb6635ed5dcc9db87e7
Ben Kolera <ben.kolera@gmail.com>
2019-03-03 08:38:56 +1000

    Editor form.

--- frontend/src/Frontend/Editor.hs
+++ frontend/src/Frontend/Editor.hs
@@ -8,6 +8,7 @@ module Frontend.Editor where
 import           Reflex.Dom.Core
 
 import           Control.Monad.Fix      (MonadFix)
+import qualified Data.Map               as Map
 import           Obelisk.Route.Frontend (Routed, RoutedT, askRoute, maybeRoute_)
 
 import           Common.Route           (DocumentSlug)
@@ -17,23 +18,37 @@ editor
      , PostBuild t m
      , MonadFix m
      , MonadHold t m
+     , Prerender js m
      )
   => RoutedT t (Maybe DocumentSlug) m ()
-editor = maybeRoute_ create edit
-
-create
-  :: ( DomBuilder t m
-     )
-  => m ()
-create = elClass "div" "editor-page" $ do
-  blank
-
-edit
-  :: ( DomBuilder t m
-     , PostBuild t m
-     , Routed t DocumentSlug m
-     )
-  => m ()
-edit = elClass "div" "editor-page" $ do
-  slugDyn <- askRoute
-  display slugDyn
+editor = elClass "div" "editor-page" $ do
+  elClass "div" "container" $
+    elClass "div" "row" $
+      elClass "div" "col-xs-12 col-md-10 offset-md-1" $ do
+        prerender blank $ el "form" $
+          el "fieldset" $ do
+            _ <- elClass "fieldset" "form-group" $
+              textInput $ def
+                & textInputConfig_attributes .~ (constDyn (Map.fromList
+                  [("class","form-control")
+                  ,("placeholder","Article Title")
+                  ]))
+            _ <- elClass "fieldset" "form-group" $
+              textInput $ def
+                & textInputConfig_attributes .~ (constDyn (Map.fromList
+                  [("class","form-control")
+                  ,("placeholder","What's this article about?")
+                  ]))
+            _ <- elClass "fieldset" "form-group" $
+              textArea $ def
+                & textAreaConfig_attributes .~ (constDyn (Map.fromList
+                  [("class","form-control")
+                  ,("placeholder","Write your article (in markdown)")
+                  ,("rows","8")
+                  ]))
+            (bElt,_) <- elClass' "button" "btn btn-lg btn-primary pull-xs-right" $ text "Publish Article"
+            let publishE = domEvent Click bElt
+            clicked <- count publishE
+            display clicked
+            pure ()
+  pure ()
