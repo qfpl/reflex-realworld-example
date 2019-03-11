b1bbf2fe7937b738a56d6b00907c24e5a8b3fdb3
Ben Kolera <ben.kolera@gmail.com>
2019-03-11 21:38:26 +1000

    Ooops. Pointing back to my github fork

--- .obelisk/impl/default.nix
+++ .obelisk/impl/default.nix
@@ -1,1 +1,9 @@
-import /home/bkolera/src/github/obsidiansystems/obelisk
+# DO NOT HAND-EDIT THIS FILE
+import ((import <nixpkgs> {}).fetchFromGitHub (
+  let json = builtins.fromJSON (builtins.readFile ./github.json);
+  in { inherit (json) owner repo rev sha256;
+       private = json.private or false;
+     }
+))
+# For local testing of my obelisk fork
+# import /home/bkolera/src/github/obsidiansystems/obelisk
